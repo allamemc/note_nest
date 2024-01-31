17ff12fbc84f1037240f5de059917b9ce1648e9b
usuario2 <usuario2@gmail.com>
2024-01-31 14:39:33 +0100

    Remove unused import statements

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useState } from 'react'
 import { UserContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion'
